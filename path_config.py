9c417bfd1f3b83838770d4ee5121746d8e8e6547
Devashish-Pisal <dpisal85@gmail.com>
2026-03-22 18:29:21 +0100

    add model finetuning output folder

--- path_config.py
+++ path_config.py
@@ -4,5 +4,5 @@ PROJECT_ROOT = Path(__file__).parent
 TRAIN_DATASET_PATH = PROJECT_ROOT / "data" / "raw" / "SROIE2019" / "train"
 TEST_DATASET_PATH = PROJECT_ROOT / "data" / "raw" / "SROIE2019" / "test"
 PROCESSED_DATA_PATH = PROJECT_ROOT / "data" / "processed"
-
+MODEL_DATA_PATH = PROJECT_ROOT / "model_data"
 
