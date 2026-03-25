d8f5f99f2ad0d6a7939b1d42980d1dee50e08d18
Devashish-Pisal <dpisal85@gmail.com>
2026-03-25 16:37:37 +0100

    finetune layoutLM model on SROIE dataset

--- src/model_finetuning/layoutLM_v3.py
+++ src/model_finetuning/layoutLM_v3.py
@@ -1,14 +1,24 @@
 from train_dataset_prep import TRAIN_DATA
 from test_dataset_prep import TEST_DATA
-from transformers import LayoutLMv3Processor, LayoutLMv3ForTokenClassification
+from transformers import LayoutLMv3Processor, LayoutLMv3ForTokenClassification, TrainingArguments, Trainer
 from dataset_util import preprocess
 from datasets import Dataset
-from path_config import PROCESSED_DATA_PATH
+from path_config import PROCESSED_DATA_PATH, MODEL_DATA_PATH
 from loguru import logger
+from huggingface_hub import login
+from dotenv import load_dotenv
+import numpy as np
 import os
 import torch
+import evaluate
+import time
 
-
+# Login to HF
+load_dotenv()
+HUGGINGFACE_TOKEN = os.environ.get("HF_WRITE_TOKEN")
+if HUGGINGFACE_TOKEN:
+    login(HUGGINGFACE_TOKEN)
+    logger.info("Hugging Face authentication successful.")
 
 # Collect unique labels from the datasets, so that we can assign them numbers (ids)
 unique_labels = set()
@@ -60,7 +70,6 @@ else:
         ENCODED_TEST_DATASET['pixel_values'].append(encodings['pixel_values'])
     torch.save(ENCODED_TEST_DATASET, encoded_test_dataset_path)
     logger.success(f"ENCODED_TEST_DATASET.pt successfully created!")
-
 ENCODED_TRAIN_DATASET = Dataset.from_dict(ENCODED_TRAIN_DATASET)
 ENCODED_TEST_DATASET = Dataset.from_dict(ENCODED_TEST_DATASET)
 
@@ -76,3 +85,68 @@ Dataset({
 })
 '''
 
+
+def compute_metrics(p):
+    """
+    Use seqeval to compute the token level F1 score.
+    """
+    predictions, labels = p
+    predictions = np.argmax(predictions, axis=-1)
+    true_predictions = []
+    true_labels = []
+    for pred_seq, label_seq in zip(predictions, labels):
+        curr_preds = []
+        curr_labels = []
+        for p_id, l_id in zip(pred_seq, label_seq):
+            if l_id == -100:
+                # ignore special tokens and padding
+                continue
+            curr_labels.append(id2label[l_id])
+            curr_preds.append(id2label[p_id])
+        true_predictions.append(curr_preds)
+        true_labels.append(curr_labels)
+
+    results = seqeval.compute(predictions=true_predictions, references=true_labels)
+    return {
+        "precision": results["overall_precision"],
+        "recall":    results["overall_recall"],
+        "f1":        results["overall_f1"],
+        "accuracy":  results["overall_accuracy"],
+    }
+
+
+#Finetune model
+seqeval = evaluate.load("seqeval") # Metric
+training_args = TrainingArguments(
+    output_dir=MODEL_DATA_PATH,
+    learning_rate=2e-5,
+    per_device_train_batch_size=2,
+    per_device_eval_batch_size=2,
+    num_train_epochs=15,
+    weight_decay=0.01,
+    logging_steps=50,
+    eval_strategy="steps",
+    eval_steps=200,
+    save_steps=200,
+    load_best_model_at_end=True,
+    metric_for_best_model="f1",
+    greater_is_better=True,
+)
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=ENCODED_TRAIN_DATASET,
+    eval_dataset=ENCODED_TEST_DATASET,
+    processing_class=processor,
+    compute_metrics=compute_metrics,
+)
+
+logger.info(f"Starting finetuning.....")
+start_time = time.time()
+trainer.train()
+end_time = time.time()
+logger.info(f"Finetuning completed!")
+logger.info(f"Finetuning took {end_time - start_time} seconds time.")
+
+trainer.save_model(os.path.join(MODEL_DATA_PATH, "layoutlmv3-sroie-final"))
+processor.save_pretrained(os.path.join(MODEL_DATA_PATH, "layoutlmv3-sroie-final"))
